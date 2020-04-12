3bea7929770944baa2719f8be8d7ff99399e0e9c
Laura Vas <v.luva99@gmail.com>
2020-04-12 12:06:11 +0200

    [WIP]
     - connection o local db is success
     - index page

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 
 var express = require('express');
+const transformMiddleware = require('express-transform-bare-module-specifiers').default;
+
 const Trip = require('./data/Trip.js');
 var oracledb = require('oracledb');
 
@@ -7,7 +9,7 @@ var oracledb = require('oracledb');
 var connect = {
    "user": "TRAINUSER",
    "password": "mavlogin",
-   "connectString": "localhost:1521/XEPDB1"
+   "connectString": "192.168.56.1:1521/XEPDB1"
 }
 
 var app = express();
@@ -15,7 +17,9 @@ var app = express();
 var trips = [new Trip(), new Trip()];
 trips[1].name = "notBela";
 
+
 app.use(express.static('site'));
+app.use('/node_modules',express.static('node_modules'));
 
 app.get('/trains', function (req, res) {
    //res.send('Hello World!');
@@ -40,17 +44,34 @@ app.get('/trips', function (req, res) {
          return;
       }
 
-      // Release the connection
-      connection.release(
-         function (err) {
-            if (err) {
-               console.error(err.message);
-            } else {
-               console.log("GET /user_profiles : Connection released");
-            }
-         });
+      connection.execute("SELECT * FROM JEGY", {}, {
+         outFormat: oracledb.OBJECT // Return the result as Object
+      }, function (err, result) {
+         if (err) {
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+               status: 500,
+               message: "Error getting the jegyek",
+               detailed_message: err.message
+            }));
+         } else {
+            res.contentType('application/json').status(200);
+            res.send(JSON.stringify(result.rows));
+         }
+         // Release the connection
+         connection.release(
+            function (err) {
+               if (err) {
+                  console.error(err.message);
+               } else {
+                  console.log("GET /user_profiles : Connection released");
+               }
+            });
+      });
    });
 });
+
+app.use(transformMiddleware());
 
 var server = app.listen(8081, function () {
    var host = server.address().address
--- src/site/js/main.js
+++ src/site/js/main.js
@@ -1,10 +1,10 @@
 
 
-$.getJSON('/trains', function(data) {
+$.getJSON('/trips', function(data) {
     data.forEach(element => {
 
         var node = document.createElement("LI");
-        var textnode = document.createTextNode(element.name);
+        var textnode = document.createTextNode(element.FELHASZNALO);
         node.appendChild(textnode);
         document.getElementById("test").appendChild(node);
     });
